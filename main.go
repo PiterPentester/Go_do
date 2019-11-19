4fa0256dd1afaaed581676fa4f511b056ab30381
Bogdan Pakhomov <shtormless@gmail.com>
2019-11-19 11:31:04 -0600

    app: main.go -> main, routers

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
   "fmt"
   "github.com/gorilla/mux"
   "net/http"
-  "encoding/json"
-  "math/rand"
-  "strconv"
 )
 
 type Task struct {
@@ -17,65 +14,17 @@ type Task struct {
 
 var tasks []Task
 
-func getTasks(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  json.NewEncoder(w).Encode(tasks)
-}
-
-func createTask(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  var task Task
-  _ = json.NewDecoder(r.Body).Decode(&task)
-  task.ID = strconv.Itoa(rand.Intn(1000000))
-  tasks = append(tasks, task)
-  json.NewEncoder(w).Encode(&task)
-}
-
-func getTask(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  params := mux.Vars(r)
-  for _, item := range tasks {
-    if item.ID == params["id"] {
-      json.NewEncoder(w).Encode(item)
-      return
-    }
-  }
-  json.NewEncoder(w).Encode(&Task{})
-}
-
-func updateTask(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  params := mux.Vars(r)
-  for index, item := range tasks {
-    if item.ID == params["id"] {
-      tasks = append(tasks[:index], tasks[index+1:]...)
-      var task Task
-      _ = json.NewDecoder(r.Body).Decode(&task)
-      task.ID = params["id"]
-      tasks = append(tasks, task)
-      json.NewEncoder(w).Encode(&task)
-      
-      return
-    }
-  }
-  json.NewEncoder(w).Encode(tasks)
-}
-
-func deleteTask(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  params := mux.Vars(r)
-  for index, item := range tasks {
-    if item.ID == params["id"] {
-      tasks = append(tasks[:index], tasks[index+1:]...)
-      break
-    }
-  }
-  json.NewEncoder(w).Encode(tasks)
-}
-
 func main() {
   fmt.Println("It works!!!")
-  router := mux.NewRouter()	
+  router := mux.NewRouter()
+  
+  db, err := sql.Open("mysql", "root:root@(127.0.0.1:3306)/mysql?parseTime=true")
+  if err != nil {
+    log.Fatal(err)
+  }
+  if err := db.Ping(); err != nil {
+    log.Fatal(err)
+  }
   
   tasks = append(tasks, Task{ID: "1", Description: "My first Task", Status: false})
   
--- routers.go
+++ routers.go
@@ -1,6 +1,65 @@
+package main
 
+import (
+  "github.com/gorilla/mux"
+  "net/http"
+  "encoding/json"
+  "math/rand"
+  "strconv"
+)
 
-func getPosts(w http.ResponseWriter, r *http.Request) {
+func getTasks(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
-  json.NewEncoder(w).Encode(posts)
+  json.NewEncoder(w).Encode(tasks)
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  var task Task
+  _ = json.NewDecoder(r.Body).Decode(&task)
+  task.ID = strconv.Itoa(rand.Intn(1000000))
+  tasks = append(tasks, task)
+  json.NewEncoder(w).Encode(&task)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  for _, item := range tasks {
+    if item.ID == params["id"] {
+      json.NewEncoder(w).Encode(item)
+      return
+    }
+  }
+  json.NewEncoder(w).Encode(&Task{})
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  for index, item := range tasks {
+    if item.ID == params["id"] {
+      tasks = append(tasks[:index], tasks[index+1:]...)
+      var task Task
+      _ = json.NewDecoder(r.Body).Decode(&task)
+      task.ID = params["id"]
+      tasks = append(tasks, task)
+      json.NewEncoder(w).Encode(&task)
+      
+      return
+    }
+  }
+  json.NewEncoder(w).Encode(tasks)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  for index, item := range tasks {
+    if item.ID == params["id"] {
+      tasks = append(tasks[:index], tasks[index+1:]...)
+      break
+    }
+  }
+  json.NewEncoder(w).Encode(tasks)
 }
